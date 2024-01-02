4ace4969a3c560fad226b3289b29faa4cf76d4ff
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-02 15:28:08 +0100

    Feat: finish task 0, 1 and 2

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+# Print the phrse given in the task
+print("\"Programming is like building a multilingual puzzle")
---
+++ 0x00-python-hello_world/main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+# Print the phrase "Best school"
+print("Best School")
