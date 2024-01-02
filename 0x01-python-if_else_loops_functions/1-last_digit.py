37f2bc4c6f07139eae96f901f349dd884609cc7b
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-02 22:01:12 +0100

    refactor: make code in 1-last_digit.py more cleaner and reuasable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 # Get the last digit of the number
 last_digit = abs(number) % 10
+prefix = f"Last digit of {number} is {last_digit} and is"
 if (last_digit) > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print(f"{prefix} greater than 5")
 elif (last_digit == 0):
-    print(f"Last digit of {number} is {last_digit} and is 0")
+    print(f"{prefix} 0")
 elif (last_digit < 6) and (last_digit) != 0:
-    print(f"Last digit of {number} is {last_digit} and is\
- less than 6 and not 0")
+    print(f"{prefix} less than 6 and not 0")
