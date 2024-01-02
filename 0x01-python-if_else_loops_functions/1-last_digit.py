cd1ce40e2a22775fde0fce77b89933d9ee1889d7
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-02 22:20:23 +0100

    feat: add support for negative random generated numbers in 1-last_digit.py file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
+if number < 0:
+    last_digit = -last_digit
 prefix = f"Last digit of {number:d} is {last_digit:d} and is"
 if (last_digit) > 5:
     print(f"{prefix} greater than 5")
