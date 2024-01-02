1193ad444f8f81b86b30e547837d7a3a16631c9a
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-02 22:24:29 +0100

    feat: add code for the 3-print_alphabt.py file
    prints the ASCII alphabet, in lowercase, not followed by a new line.
    Print all the letters except q and e
    You can only use one print function with string format
    You can only use one loop in your code
    You are not allowed to store characters in a variable

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# Print the ASCII alphabet in lowercase without a new line
+for char in range(ord('a'), ord('z') + 1):
+    if chr(char) not in {'q', 'e'}:
+        print(f"{chr(char)}", end='')
