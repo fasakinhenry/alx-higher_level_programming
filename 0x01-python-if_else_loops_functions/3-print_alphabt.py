2ad05c679ab7e45f79fdb34ae2ff3bf7fe188129
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-02 22:26:10 +0100

    refactor: use format instead of f-strings in 3-print_alphabt.py file

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@
 # Print the ASCII alphabet in lowercase without a new line
 for char in range(ord('a'), ord('z') + 1):
     if chr(char) not in {'q', 'e'}:
-        print(f"{chr(char)}", end='')
+        print("{}".format(chr(char)), end='')
