280e2e33796332ee1d497204d5aff4ba279feccb
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-02 23:10:53 +0100

    style: refactore code in 6-print_comb3.py to follow PEP or pycodestyle standards

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@
 # Print all possible different combinations of two digits
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, ones_digit), end=", " 
-	if tens_digit != 8 or ones_digit != 9 else "\n")
+        print("{:d}{:d}".format(tens_digit, ones_digit), end=", "
+              if tens_digit != 8 or ones_digit != 9 else "\n")
