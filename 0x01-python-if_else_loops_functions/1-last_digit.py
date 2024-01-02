e909f0a20d28825187f3b52bac575c5c921d9f7d
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-02 21:57:12 +0100

    style: make 1-last_digit.py pycodestyle standard

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,4 +8,5 @@ if (last_digit) > 5:
 elif (last_digit == 0):
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif (last_digit < 6) and (last_digit) != 0:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} and is\
+ less than 6 and not 0")
