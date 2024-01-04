a881391855518306b7595d2b0a1724802b9f64db
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-04 16:28:25 +0100

    docs: add comments for the 1-calculation.py file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5
+    Import from the calculator module"""
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
