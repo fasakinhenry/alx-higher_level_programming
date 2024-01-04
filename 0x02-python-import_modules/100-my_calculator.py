14d1ec6e4de44cc128b9d71346d4d701f01adb76
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-04 20:53:26 +0100

    fix: add exit with status 1 for the operator if condition in 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
 
     if operator not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
     else:
         if operator == '+':
             result = add(a, b)
