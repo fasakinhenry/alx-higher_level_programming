1bc782e9ac5fdca63321aeb6c16bc1f4fc192b47
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-04 18:07:17 +0100

    feat: add files 5-variable_load.py and variable_load_5.py
    imports a variable a nad prints its value
    You are not allowed to use * for importing or __import__
    Your code should not be executed when imported
    guillaume@ubuntu:~/0x02$ cat variable_load_5.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Check if __name__ is equal to __main__
+if __name__ == "__main__":
+    """imports the vrable a and prints its value"""
+    import variable_load_5 as vload
+
+    print("{}".format(vload.a))
+
