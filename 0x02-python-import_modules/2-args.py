19be2c6bbd09b413bd750abd7182ff2c4d08b5ab
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-04 16:34:43 +0100

    add file 2-args.py for task 2
    prints the number of and the list of its arguments.
    The output should be:
    Number of argument(s) followed by argument (if number is one) or arguments (otherwise), followed by
    : (or . if no arguments were passed) followed by
    a new line, followed by (if at least one argument),
    one line per argument:
    the position of the argument (starting at 1) followed by :, followed by the argument value and a new line
    Your code should not be executed when imported
    The number of elements of argv can be retrieved by using: len(argv)

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    num_args = len(argv) - 1
+    args_str = "arguments" if num_args != 1 else "argument"
+
+    print("{} {}.".format(num_args, args_str))
+
+    if num_args > 0:
+        for i in range(1, num_args + 1):
+            print("{}: {}".format(i, argv[i]))
