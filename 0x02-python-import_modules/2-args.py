9529414945094f332876bb0f2c6bbe6df497f5a5
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-04 16:43:15 +0100

    style: make code in 2-args.py adhere to the pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     num_args = len(argv) - 1
     args_str = "arguments" if num_args != 1 else "argument"
 
-    print("{} {}.".format(num_args, args_str) if num_args < 1 else "{} {}:".format(num_args, args_str))
+    print("{} {}.".format(num_args, args_str) if num_args < 1
+          else "{} {}:".format(num_args, args_str))
 
     if num_args > 0:
         for i in range(1, num_args + 1):
