7c26b96263260e20fbed71374ecc93b4387e1970
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-05 05:21:02 +0100

    feat: add the reversed function to the 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,5 @@
 
 # Prints all integers of a list, in reverse order
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in my_list:
+    for i in reversed(my_list):
         print("{:d}".format(i))
