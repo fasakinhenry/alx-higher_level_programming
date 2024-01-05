f8e3b0571f2c56f57396ff3834f0437f33eb2e1f
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-05 05:35:50 +0100

    docs: remove the comment in 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-""" replaces an element in a list at a specific position
-without modifying the original list (like in C) """
-
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list.copy()
