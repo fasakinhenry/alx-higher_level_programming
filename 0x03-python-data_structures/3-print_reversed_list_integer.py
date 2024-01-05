1e0352911d8e405d2fa6d3f61a7575b1a31da622
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-05 03:54:05 +0100

    feat: add function new_in_list
    This commit introduces a new function new_in_list, which replaces an element at a specific position in a list without modifying the original list. The function checks for negative indices and indices out of range, returning a copy of the original list in such cases.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,6 @@
 
 # Retrieves an element from a list like in C
 def print_reversed_list_integer(my_list=[]):
-    if idx not in range(0, len(my_list)):
+    my_list.reverse()
+    for i in range(0, len(my_list)):
+        print(my_list[i])
