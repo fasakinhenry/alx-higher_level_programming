1b884e8469896c5ddded9710b536a0686c279520
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-05 06:55:59 +0100

    refactor: use direct assignment with += operator

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,8 +2,8 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     # Ensure each tuple has at least two elements, filling with 0 if necessary
-    tuple_a = tuple_a + (0, 0)
-    tuple_b = tuple_b + (0, 0)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
 
     # Return a tuple with the sum of corresponding
     # elements from tuple_a and tuple_b
