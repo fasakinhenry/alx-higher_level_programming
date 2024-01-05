8439e91b740c340c5203cb390a91ee08cb752c8f
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-05 04:48:37 +0100

    fix: fix range of for loop in 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,4 +4,4 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     for i in range(0, len(my_list)):
-        print("{:}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
