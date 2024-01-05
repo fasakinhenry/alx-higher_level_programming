72ef15a7fbd75aabd7a66bb548073d8f7bf5a012
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-05 06:54:51 +0100

    style: make code in 7-add_tuple.py align to pycodestyle

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,5 +5,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a = tuple_a + (0, 0)
     tuple_b = tuple_b + (0, 0)
 
-    # Return a tuple with the sum of corresponding elements from tuple_a and tuple_b
+    # Return a tuple with the sum of corresponding
+    # elements from tuple_a and tuple_b
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
