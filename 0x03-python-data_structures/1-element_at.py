c6beaaf982eca2f2a60f94bc534d7e31fe7b2101
Henry Fasakin <fasakinhenry@gmail.com>
2024-01-06 22:12:02 +0100

    fix: change condition of if statement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
-#!/usr/binpython3
-
+#!/usr/bin/python3
 # Retrieves an element from a list like in C
 def element_at(my_list, idx):
-    if idx > 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
-    return my_list[idx]
+    else:
+        return my_list[idx]
+
