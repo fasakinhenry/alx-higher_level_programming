e02bc64fda8710a65dbc91a3669876d073ab2e4b
Henry Fasakin <fasakinhenry@gmail.com>
2024-01-06 18:11:34 +0100

    refactor: change 1-main.ly if condition

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 
 # Retrieves an element from a list like in C
 def element_at(my_list, idx):
-    if idx not in range(0, len(my_list)):
+    if idx > 0 or idx > len(my_list):
         return None
     return my_list[idx]
