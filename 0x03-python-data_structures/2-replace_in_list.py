07aa921fa2025f6b0cd35e9fcb454c9e8fd4d311
Henry Fasakin <fasakinhenry@gmail.com>
2024-01-06 22:23:19 +0100

    refactor: change code if condition statement

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,6 +3,6 @@
 # Replaces an element of a list at a specific position (like in C).
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        return None
+        return
     my_list[idx] = element
     return my_list
