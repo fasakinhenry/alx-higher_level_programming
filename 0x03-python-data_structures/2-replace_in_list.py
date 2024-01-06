f3121fa84d7db99a8d21dc02ad8680033ff35c6b
Henry Fasakin <fasakinhenry@gmail.com>
2024-01-06 22:17:36 +0100

    fix: fix code condition

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,6 +3,6 @@
 # Replaces an element of a list at a specific position (like in C).
 def replace_in_list(my_list, idx, element):
     if idx not in range(0, len(my_list)):
-        return my_list
+        return None
     my_list[idx] = element
     return my_list
