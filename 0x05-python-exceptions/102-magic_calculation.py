7b907ff552cec06fada5c9cf3779f364bd8e6b6b
Henry Fasakin <fasakinhenry@gmail.com>
2024-01-22 23:58:35 +0100

    feat: add file 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -7,8 +6,8 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             else:
-                result += (a ** b) / i
+                result += a ** b / i
         except:
             result = b + a
             break
-    return (result)
+    return
