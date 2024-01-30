cc8b672cf82c65c73ad95e01835e445e4021a16a
Fasakin Henry <fasakinhenry@gmail.com>
2024-01-30 09:20:43 +0100

    docs: add updates for the 101-nqueeens.p file

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
         if (already_exists(y)):
             return False
         i = 0
-        while(i < x):
+        while (i < x):
             if abs(a[i][1] - y) == abs(i - x):
                 return False
             i += 1
