cfd74b0555e58640a4d860f33e2ab8eb02d2d6bb
Fasakin Henry <fasakinhenry@gmail.com>
2024-02-02 02:48:36 +0100

    add the required files

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-"""text indent"""
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    """text indent"""
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    for delimeter in "?:.":
-        words = (delimeter + "\n\n").join(
-                [index.strip(" ") for index in words.split(delimeter)])
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
 
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/5-text_indentation.txt")
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
