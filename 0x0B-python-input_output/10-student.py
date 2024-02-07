741c057d14f0812dc07a19f080a1830eb3f4b900
Fasakin Henry <fasakinhenry@gmail.com>
2024-02-07 01:10:42 +0100

    feat: add new features to 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
-"""A student class that defines a student by
-Public instances attributes:
-first_name, last_name, age
-"""
+"""Defines a class Student."""
 
 
 class Student:
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if (isinstance(attrs, list) and all(type(ele) is str for ele in attrs)):
-            attributes = {}
-            for attribute_name in attrs:
-                if (hasattr(self, attribute_name)):
-                    attribute_value = getattr(self, attribute_name)
-                    attributes[attribute_name] = attribute_value
-            return attributes
-        else:
-            return self.__dict__
+        """Get a dictionary representation of the Student.
+
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
