16f754fcfec8a41e3b1c479d26d68307f7bac585
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 00:42:22 +0100

    Add code to print 3 lines using array of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Prints 3 lines but uses an array of string and loop
+let string = ["C is fun", "Python is cool", "Javascript is amazing"];
+
+for (let index = 0; index <= string.length; index++) {
+  console.log(string[index]);
+}
