6cd55f1c7622f040340acba4c145074c3dbc9a8e
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 00:48:50 +0100

    Fix loop logical comparison

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,6 +3,6 @@
 
 let string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let index = 0; index <= string.length; index++) {
+for (let index = 0; index < string.length; index++) {
   console.log(string[index]);
 }
