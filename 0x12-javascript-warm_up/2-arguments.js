b96de02ca61d32a40e27a25d30e3f04216b714c2
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 00:09:18 +0100

    Fix typo in the code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 // Prints a message depending on the number of arguments passed
 
 if (process.argv.length === 2) {
-    console.log('No arguments');
+    console.log('No argument');
 } else if (process.argv.length === 3) {
     console.log('Argument found');
 } else {
