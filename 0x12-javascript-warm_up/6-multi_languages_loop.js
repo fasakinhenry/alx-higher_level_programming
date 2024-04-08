90cb00fa28a7c8b4862a33d5d4ae7fa0b2128adc
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 00:54:48 +0100

    Change the let keyword to const since it is a constant

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Prints 3 lines with array of str & loop
 
-let language = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const language = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let index = 0; index < 3; index++) {
   console.log(language[index]);
