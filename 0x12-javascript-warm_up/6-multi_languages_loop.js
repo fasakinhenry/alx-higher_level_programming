8dfa9f599a3f24e9f040119e7c03ed6732168e20
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 00:47:18 +0100

    Change double quotes for string to single quotes

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Prints 3 lines but uses an array of string and loop
 
-let string = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let index = 0; index <= string.length; index++) {
   console.log(string[index]);
