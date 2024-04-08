43b6ac88c8363e1a2a059bc4ccaa28b29a0fd5c3
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 00:39:01 +0100

    Fix typo and indentation in the code

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 // Only prints if the first argument can be converted to an integer:
 
 if (isNaN(process.argv[2])) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log('My number' + parseInt(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
