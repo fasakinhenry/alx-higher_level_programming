3830845f01875e935e754f5cafc5e8b05ecc556f
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 00:03:17 +0100

    Use single quotes instead of double quotes

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // Script that prints three lines
 
-console.log("C is fun\nPython is cool\nJavaScript is amazing");
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
