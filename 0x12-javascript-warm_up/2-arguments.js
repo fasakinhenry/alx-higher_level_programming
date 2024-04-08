9ce39e8d37056bc8ba1f6fa4b30e751fdee6912f
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 00:18:01 +0100

    Fix indentation issues and use two spaces instead of tabs

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 // Prints a message depending on the number of arguments passed
 
 if (process.argv.length === 2) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
