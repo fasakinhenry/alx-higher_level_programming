f54f6d950ad1c6e89437686114dd0bcf72eb06a4
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 00:32:59 +0100

    Add semicolon

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // Prints 2 arguments passed to it is keyword in the middle
 
-console.log(process.argv[2] + ' is ' + process.argv[3])
+console.log(process.argv[2] + ' is ' + process.argv[3]);
