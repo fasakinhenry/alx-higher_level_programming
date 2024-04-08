7b76075a0a466262c236164651e290dce8774331
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 00:50:30 +0100

    Change the name of the variable in the code

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Prints 3 lines but uses an array of string and loop
 
-let string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let language = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let index = 0; index < string.length; index++) {
-  console.log(string[index]);
+  console.log(language[index]);
 }
