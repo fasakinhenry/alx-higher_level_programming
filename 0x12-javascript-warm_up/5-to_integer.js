20a25093fb27abe0a0e4c8ef21d936fbd7ac278e
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 00:37:45 +0100

    prints My number: <first argument converted in integer>
    Only prints if the first argument can be converted to an integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// prints My number: <first argument converted in integer>
+// Only prints if the first argument can be converted to an integer:
+
+if (isNaN(process.argv[2])) {
+    console.log('Not a number');
+} else {
+    console.log('My number' + parseInt(process.argv[2]));
+}
