f62b5932457917f3755a306f0bba9f9f190b0565
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 01:16:48 +0100

    Fix issues with conditionals

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 // Print x times "C is fun"
 
-if (isNaN(process.argv)) {
+const textOutput = 'C is fun';
+
+if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   for (let index = 0; index < parseInt(process.argv[2]); index++) {
-    console.log('C is fun');
+    console.log(textOutput);
   }
 }
