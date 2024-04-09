5f13a0043c2fa4649e92480b4ba9f860170649df
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 02:45:46 +0100

    Fix indentation error

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -3,14 +3,14 @@
 const myObject = {
     type: 'object',
     value: 12
-  };
-  console.log(myObject);
-  myObject.incr = function () {
-    this.value++;
-  };
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
