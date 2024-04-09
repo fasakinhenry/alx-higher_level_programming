eb1a7153e42db127b956891ebe90c075a4011913
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 02:19:44 +0100

    Add script to replace the value 12 to 89 in object

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Replace the value 12 to 89 in object
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
