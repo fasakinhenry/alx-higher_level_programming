df6cee7c3a25442d500f56cefe5505bc402ce49c
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 01:29:44 +0100

    Add a function to add integers passed as arguments

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Prints the addition of 2 integers
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+let a = parseInt(process.argv[2]);
+let b = parseInt(process.argv[3]);
+
+add(a, b);
