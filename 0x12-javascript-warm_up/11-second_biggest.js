8adcc6c205b336c45f8c47cc3f4ce755d2571329
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 02:01:23 +0100

    edit comment

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Searches the second biggest integer in the list of the arguments
+// Searches the second biggest integer in the list of arguments
 
 if (process.argv.length <= 3) {
   console.log(0);
