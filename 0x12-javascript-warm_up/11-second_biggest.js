b317dde23626cba9d4539554eea077ce398791a9
Fasakin Henry <fasakinhenry@gmail.com>
2024-04-09 01:50:33 +0100

    Add code to find the second biggest integer in the list of comments

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Searches the second biggest integer in the list of the arguments
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const list = process.argv.sort();
+  console.log(list.reverse()[1]);
+}
